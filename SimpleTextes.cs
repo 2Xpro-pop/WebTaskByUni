c8c470f9d5ab5da601dfb03a456c394ea159175e
2Xpro-pop <bay.asanov@mail.ru>
2022-10-06 16:51:50 +0600

    Добавьте файлы проекта.

---
+++ SimpleTextes.cs
@@ -0,0 +1,10 @@
+﻿namespace First
+{
+    public static class Simples
+    {
+        public static string LoremIpsum => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum autem facere aut, ipsa voluptate dicta aliquid consequuntur est ipsum aspernatur quam sed rem cupiditate vitae repudiandae doloremque nemo nostrum dolor! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, doloribus corrupti facere obcaecati non aliquam quod minima possimus cupiditate. Ipsa laboriosam, maxime fugiat libero aut ex soluta ipsum blanditiis nisi.";
+        public static string ImageUrl1 => "https://interier-foto.ru/wp-content/uploads/dlinnye-foto-4.jpg";
+        public static string ImageUrl2 => "https://klike.net/uploads/posts/2021-04/1618816243_2.jpg";
+
+    }
+}
